bc7bfcbcc3e4e94416447d41ece047be763a2c1f
Ajeng Wahyuningtyas <ningtyasajeng24@gmail.com>
2022-04-16 17:27:55 +0800

    update controller

--- app/Http/Controllers/TodoListController.php
+++ app/Http/Controllers/TodoListController.php
@@ -1,25 +1,58 @@
 <?php
 
 namespace App\Http\Controllers;
+use Illuminate\Support\Facades\DB;
 
 use Illuminate\Http\Request;
 
 class TodoListController extends Controller
 {
     public function index(){
-        return view('index');
+        $todos = DB::table('todo_lists')->get();
+        return view('app', ['todo_lists' => $todos]);
     }
 
-    public function addTask(){
-        return view('index');
+    // public function addTask(){
+    //     return view('index');
+    // }
+
+    public function saveTask(Request $request){
+        
+        // validate the form
+        $request->validate([
+            'name' => 'required|max:200'
+        ]);
+
+        // store the data
+        DB::table('todo_lists')->insert([
+            'name' => $request->name
+        ]);
+
+        return redirect()->route('index-home')->with('status', 'Task added!');
+        // return redirect()->route('pegawai.krs-index')->with('success', 'KRS Telah Ditambahkan');
+    }
+    
+    public function updateTask(Request $request, $id){
+        // validate the form
+        $request->validate([
+            'name' => 'required|max:200'
+        ]);
+
+        // update the data
+        DB::table('todo_lists')->where('id', $id)->update([
+            'name' => $request->name
+        ]);
+        
+        // redirect
+        return redirect()->route('index-home')->with('status', 'Task updated!');
     }
 
-    public function saveTask(){
-        return view('index');
-    }
+    public function deleteTask($id){
+        // delete the todo
+        DB::table('todo_lists')->where('id', $id)->delete();
 
-    public function deleteTask(){
-        return view('index');
+        // redirect
+        return redirect()->route('index-home')->with('status', 'Task removed!');
     }
 
     public function editTask(){
